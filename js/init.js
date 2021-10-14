00534211d70721d194d28e5ee23ffcf95f855970
TomoyasuTaka <piano.cembalo.taka.2nd@gmail.com>
2021-10-14 16:24:20 +0900

    波紋の模様が表示される

--- js/init.js
+++ js/init.js
@@ -4,5 +4,25 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
+    // ココ以下は波紋生成
+    $('body').ripples({
+      resolution: 1024,
+      dropRadius: 20,
+      perturbance: 0.04,
+    });
+
+
   }); // end of document ready
+
+  setInterval(function () {
+    var $el = $('body');
+    var x = Math.random() * $el.outerWidth();
+    var y = Math.random() * $el.outerHeight();
+    var dropRadius = 20;
+    var strength = 0.04 + Math.random() * 0.04;
+
+    $el.ripples('drop', x, y, dropRadius, strength);
+  }, 400);
+
 })(jQuery); // end of jQuery name space
+
